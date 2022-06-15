7669a4d162ecf1690df6e9566585f0641029450c
vlad-iancu <iancuvladalexandru@gmail.com>
2022-06-15 22:25:41 +0300

    InstallPython.sh

--- Scripts/InstallBoost.sh
+++ Scripts/InstallBoost.sh
@@ -3,8 +3,6 @@
 #In order to run this script standalone the variables SCRIPTS, PROJECT_DIR and TEMP_DIR need to be set
 BOOST_VERSION="1_78_0"
 
-source $SCRIPTS/Common.sh
-
 BOOST_ROOT=$PROJECT_DIR/BuildDependencies/boost
 
 BOOST_SRC_URL="https://boostorg.jfrog.io/artifactory/main/release/$(echo $BOOST_VERSION | tr '_' '.')/source/boost_$BOOST_VERSION.tar.bz2"
@@ -17,7 +15,7 @@ mkdir -p $TEMP_DIR
 
 cd $TEMP_DIR
 print "Downloading source"
-wget $BOOST_SRC_URL 2>> $TEMP_DIR/Init.log 1>> $TEMP_DIR/Init.log
+wget $BOOST_SRC_URL 
 
 print "Inflating source"
 tar --bzip2 -xf $BOOST_SRC
@@ -26,14 +24,14 @@ print "Configuring"
 cd "$TEMP_DIR/boost_$BOOST_VERSION"
 
 PWD=$(pwd)
-$PWD/bootstrap.sh --prefix=$BOOST_ROOT 2>> $TEMP_DIR/Init.log 1>> $TEMP_DIR/Init.log
+$PWD/bootstrap.sh --prefix=$BOOST_ROOT 
 
 
 print "Compiling"
 CORES=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
-$PWD/b2 -j ${CORES} 2>> $TEMP_DIR/Init.log 1>> $TEMP_DIR/Init.log
+$PWD/b2 -j ${CORES} 
 
 
 print "Installing"
-$(pwd)/b2 install 2>> $TEMP_DIR/Init.log 1>> $TEMP_DIR/Init.log
+$(pwd)/b2 install 
 success "Boost Successfully Installed"
